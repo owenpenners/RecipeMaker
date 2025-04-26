aac3281054936123af3a5cdabc2334b29d95984a
Dalightz <johlsunny@gmail.com>
2025-04-25 19:39:27 -0700

    Added Login, Logout, Register, & User Functionalities

---
+++ forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+class LoginForm(FlaskForm):
+    username     = StringField('Username', validators=[DataRequired()])
+    password     = PasswordField('Password', validators=[DataRequired()])
+    remember_me  = BooleanField('Remember me')
+    submit       = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email    = StringField('Email',    validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    confirm  = PasswordField('Confirm Password',
+                             validators=[DataRequired(), EqualTo('password')])
+    submit   = SubmitField('Register')
