66005dd090b0205ce0531e2564c857d767acb289
jhzhgg <david.zhang@sjsu.edu>
2025-05-11 03:15:48 -0700

    updated apps forms and view to have comment and rate

--- forms.py
+++ forms.py
@@ -1,18 +1,25 @@
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, NumberRange
 
 class LoginForm(FlaskForm):
-    username     = StringField('Username', validators=[DataRequired()])
-    password     = PasswordField('Password', validators=[DataRequired()])
-    remember_me  = BooleanField('Remember me')
-    submit       = SubmitField('Sign In')
-
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    email    = StringField('Email',    validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
-    confirm  = PasswordField('Confirm Password',
-                             validators=[DataRequired(), EqualTo('password')])
-    submit   = SubmitField('Register')
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+class RatingForm(FlaskForm):
+    value = IntegerField('Rating (1–5)', validators=[DataRequired(), NumberRange(min=1, max=5)])
+    submit = SubmitField('Submit Rating')
+
+class CommentForm(FlaskForm):
+    text = TextAreaField('Comment', validators=[DataRequired()])
+    submit = SubmitField('Post Comment')
