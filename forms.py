dea3fb906cdfe2b2ac901e8fbd0ceeb29b8dfdb3
jhzhgg <david.zhang@sjsu.edu>
2025-05-11 03:29:47 -0700

    Update forms.py with rate and comment

--- forms.py
+++ forms.py
@@ -1,20 +1,25 @@
-
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, TextAreaField
-from wtforms.validators import DataRequired, Email, EqualTo, NumberRange
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    username     = StringField('Username', validators=[DataRequired()])
+    password     = PasswordField('Password', validators=[DataRequired()])
+    remember_me  = BooleanField('Remember me')
+    submit       = SubmitField('Sign In')
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
+    email    = StringField('Email',    validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    confirm  = PasswordField('Confirm Password',
+                             validators=[DataRequired(), EqualTo('password')])
+    submit   = SubmitField('Register')
+
+
+from wtforms import IntegerField, TextAreaField
+from wtforms.validators import NumberRange
 
 class RatingForm(FlaskForm):
     value = IntegerField('Rating (1–5)', validators=[DataRequired(), NumberRange(min=1, max=5)])
